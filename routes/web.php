3d6bb1cd4be0336a3fb678f62d30e951f634e5a4
nadir inab <nadir.inab.dev@gmail.com>
2023-02-06 17:27:35 +0100

    routing is well added and user can route easily

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,11 @@ use App\Http\Controllers\EmployeesController;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::get('/', function(){
-    return redirect('/employees') ;
+    return redirect('/home/employees') ;
 });
 
-Route::get('/employees', [EmployeesController::class, 'index']);
+Route::get('/home/employees', [EmployeesController::class, 'index']);
 
 Route::get('employees/{id}',[EmployeesController::class, 'show'] ) ;
 
@@ -28,8 +27,6 @@ Route::get('/About', function(){
 Route::get('/add', function(){
     return view('save') ;
 }) ;
-
-// Route::post('delete/{id}', [EmployeesController::class, 'store']) ;
 
 Auth::routes();
 
