f44249997a1c551993b4da88ed3a100e0381d9e3
nadir inab <nadir.inab.dev@gmail.com>
2023-02-08 17:29:33 +0100

    save form is well stored and data is cool and well checked and validated

--- resources/views/save.blade.php
+++ resources/views/save.blade.php
@@ -9,22 +9,30 @@
             <div class="form-group p-1">
               <label>Name</label>
               <input name="name" type="text" class="form-control" placeholder="Enter email">
+              <span class="text-danger border-bottom" > @error('name'){{$message}} @enderror</span>
             </div>
             <div class="form-group p-1">
               <label>Role</label>
               <input name="role" type="text" class="form-control" placeholder="Enter email">
+              <span class="text-danger border-bottom"> @error('role'){{$message}} @enderror</span>
+
             </div>
             <div class="form-group p-1">
               <label>City</label>
               <input name="city" type="text" class="form-control" placeholder="Enter email">
+              <span class="text-danger border-bottom"> @error('city'){{$message}} @enderror</span>
+
             </div>
             <div class="form-group p-1">
               <label>Image</label>
-              <input name="image" type="text" class="form-control" placeholder="Upload Image">
+              <input name="image" type="file" class="form-control" placeholder="Upload Image">
+
             </div>
             <div class="form-group p-1">
               <label for="exampleInputPassword1">Linkdin Account</label>
-              <input name="linkdin" type="text" class="form-control" id="exampleInputPassword1" placeholder="Password">
+              <input name="linkdin" type="text" class="form-control"placeholder="Linkdin account">
+              <span class="text-danger border-bottom"> @error('linkdin'){{$message}} @enderror</span>
+
             </div>
             <button type="submit" class="btn btn-success p-2 m-auto d-block mt-2 w-25"><small class="fw-bold "> + </small>Add</button>
           </form>
