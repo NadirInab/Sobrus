9e6ea252add92b170fd5161619849c9de7545d41
nadir inab <nadir.inab.dev@gmail.com>
2023-02-09 19:55:26 +0100

    few lines has been added

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -14,24 +14,25 @@
               <label>Role</label>
               <input name="role" type="text" class="form-control" placeholder="Enter email" value="{{$employee->role}}">
               <span class="text-danger border-bottom">@error('role'){{$message}} @enderror</span>
-
             </div>
+
             <div class="form-group">
               <label>City</label>
               <input name="city" type="text" class="form-control" placeholder="Enter email" value="{{$employee->city}}">
               <span class="text-danger border-bottom">@error('city'){{$message}} @enderror</span>
-
             </div>
+
             <div class="form-group">
               <label>Image</label>
               <input name="image" type="file" class="form-control" placeholder="Upload Image" value="{{$employee->imag}}">
             </div>
+
             <div class="form-group">
               <label for="exampleInputPassword1">Linkdin Account</label>
               <input name="linkdin" type="text" class="form-control" id="exampleInputPassword1" value="{{$employee->linkdin}}">
               <span class="text-danger border-bottom">@error('linkdin'){{$message}} @enderror</span>
-
             </div>
+            
             <button type="submit" class="btn btn-secondary m-auto d-block p-2 mt-2 w-25">UpDate</button>
           </form>
     </div>
